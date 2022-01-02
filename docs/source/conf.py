9b33369da13173e52eae5611f0b044dc70daa696
Jibril-Frej <frejjibril@gmail.com>
2022-01-02 17:01:45 +0100

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../irspdf'))
+sys.path.append(os.path.join('..', '..', 'irspdf'))
 
 
 # -- Project information -----------------------------------------------------
